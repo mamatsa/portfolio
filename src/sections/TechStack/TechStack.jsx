9ddbcd43ab551fe0bdd8f72bba8e364ad0895cc9
otomamatsashvili <otar.mamatsashvili@gmail.com>
2023-04-09 14:41:41 +0400

    feat: attach website links to tech stack logos

--- src/sections/TechStack/TechStack.jsx
+++ src/sections/TechStack/TechStack.jsx
@@ -30,22 +30,63 @@ const TechStack = ({ techStackRef }) => {
         Technologies I’ve been working with recently
       </p>
       <div className='flex flex-wrap justify-center items-center my-10 gap-5'>
-        <img src={HTMLLogo} alt='react' />
-        <img src={CSSLogo} alt='react' />
-        <img src={JSLogo} alt='react' />
-        <img src={TSLogo} alt='react' />
-        <img src={ReactLogo} alt='react' />
-        <img src={TailwindLogo} alt='react' />
-        <img src={NodeLogo} alt='react' />
-        <img src={MongoLogo} alt='react' />
-        <img src={LinuxLogo} alt='react' />
-        <img src={FigmaLogo} alt='react' />
-        <img src={VscodeLogo} alt='react' />
-        <img src={GithubLogo} alt='react' />
-        <img src={GitLogo} alt='react' />
-        <img src={SwaggerLogo} alt='react' />
-        <img src={CypressLogo} alt='react' />
-        <img src={ExpressLogo} alt='react' />
+        <a
+          href='https://developer.mozilla.org/en-US/docs/Web/HTML'
+          target='_blank'
+        >
+          <img src={HTMLLogo} alt='html' />
+        </a>
+        <a
+          href='https://developer.mozilla.org/en-US/docs/Web/CSS'
+          target='_blank'
+        >
+          <img src={CSSLogo} alt='css' />
+        </a>
+        <a
+          href='https://developer.mozilla.org/en-US/docs/Web/JavaScript'
+          target='_blank'
+        >
+          <img src={JSLogo} alt='js' />
+        </a>
+        <a href='https://www.typescriptlang.org/' target='_blank'>
+          <img src={TSLogo} alt='ts' />
+        </a>
+        <a href='https://react.dev/' target='_blank'>
+          <img src={ReactLogo} alt='react' />
+        </a>
+        <a href='https://nodejs.org/en' target='_blank'>
+          <img src={NodeLogo} alt='node' />
+        </a>
+        <a href='https://expressjs.com/' target='_blank'>
+          <img src={ExpressLogo} alt='express' />
+        </a>
+        <a href='https://www.mongodb.com/' target='_blank'>
+          <img src={MongoLogo} alt='mongo' />
+        </a>
+        <a href='https://tailwindcss.com/' target='_blank'>
+          <img src={TailwindLogo} alt='tailwind' />
+        </a>
+        <a href='https://git-scm.com/' target='_blank'>
+          <img src={GitLogo} alt='git' />
+        </a>
+        <a href='https://github.com/' target='_blank'>
+          <img src={GithubLogo} alt='github' />
+        </a>
+        <a href='https://www.linux.org/' target='_blank'>
+          <img src={LinuxLogo} alt='linux' />
+        </a>
+        <a href='https://code.visualstudio.com/' target='_blank'>
+          <img src={VscodeLogo} alt='vscode' />
+        </a>
+        <a href='https://www.figma.com/' target='_blank'>
+          <img src={FigmaLogo} alt='figma' />
+        </a>
+        <a href='https://swagger.io/' target='_blank'>
+          <img src={SwaggerLogo} alt='swagger' />
+        </a>
+        <a href='https://www.cypress.io/' target='_blank'>
+          <img src={CypressLogo} alt='cypress' />
+        </a>
       </div>
     </section>
   );
