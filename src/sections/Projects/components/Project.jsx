104029c9849a68712496231836dcd90c7957184d
otomamatsashvili <otar.mamatsashvili@gmail.com>
2023-04-10 15:27:36 +0400

    style: make project cards more responsive

--- src/sections/Projects/components/Project.jsx
+++ src/sections/Projects/components/Project.jsx
@@ -12,27 +12,28 @@ const Project = ({
   siteLink,
 }) => {
   return (
-    <div className='bg-white rounded-lg md:w-[31%]'>
+    <div className='bg-white rounded-lg w-2/3 md:w-[31%]'>
       <img src={image} alt='name' className='w-full rounded-t-lg' />
       <div className='p-3 md:p-8'>
-        <h3 className='text-lg mb-4 md:text-2xl'>{name}</h3>
-        <p className='font-light text mb-5 md:text-lg'>{description}</p>
-        <p>
-          Tech stack: <span className='text-sm font-light'>{techStack}</span>
+        <h3 className='text-lg mb-3 mx-6 whitespace-nowrap md:text-2xl md:mb-4'>
+          {name}
+        </h3>
+        <p className='font-light text-sm mb-3 md:mb-5 md:text-lg'>
+          {description}
         </p>
-        <div className='flex items-center justify-between mt-5'>
-          <div>
-            <a href={githubLink} className='underline'>
-              <FontAwesomeIcon icon={faGithub} className=' mr-1' />
-              View Code
-            </a>
-          </div>
-          <div>
-            <a href={siteLink} className='underline'>
-              <FontAwesomeIcon icon={faLink} className='ml-10 mr-1' />
-              Live Preview
-            </a>
-          </div>
+        <p className='text-sm md:text-base'>
+          Tech stack:
+          <span className='text-xs font-light md:text-sm'>{techStack}</span>
+        </p>
+        <div className='flex items-center justify-between mt-3 md:mt-5'>
+          <a href={githubLink} className='underline flex items-center'>
+            <FontAwesomeIcon icon={faGithub} className='mr-1' />
+            <p className='hidden md:block'>View Code</p>
+          </a>
+          <a href={siteLink} className='underline flex items-center'>
+            <FontAwesomeIcon icon={faLink} className='ml-10 mr-1' />
+            <p className='hidden md:block'>Live Preview</p>
+          </a>
         </div>
       </div>
     </div>
