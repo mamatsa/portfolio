62fdd91879f86c8262a28b40eefc26892e695c8e
otomamatsashvili <otar.mamatsashvili@gmail.com>
2023-06-05 15:27:42 +0400

    feat: save language state in localstorage

--- src/navbar/components/LanguageChanger.jsx
+++ src/navbar/components/LanguageChanger.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 
 const LanguageChanger = (props) => {
   const { i18n } = useTranslation();
 
+  useEffect(() => {
+    if (localStorage.getItem('language') === 'ka') {
+      i18n.changeLanguage('ka');
+    }
+  }, []);
+
   const languageChangeHandler = () => {
     if (i18n.language === 'en') {
       i18n.changeLanguage('ka');
+      localStorage.setItem('language', 'ka');
     } else {
       i18n.changeLanguage('en');
+      localStorage.setItem('language', 'en');
     }
   };
 
